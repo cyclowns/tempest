d769d572c93555f65235706e288ed0da69bb2f60
cyclowns <notzombiedude@gmail.com>
2019-12-26 16:30:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# tempest
+
+A simple CLI application for Windows/Linux that allows you to create projects based on templates
+
+## Installation
+
+### Windows
+
+blah blah
+
+### Linux
+
+blah blah
+
+## Usage
+
+```bash
+$ tempest new {template_name or github repo} 
+```
+
+i.e.
+
+```bash
+$ tempest new npm
+... (clones default npm project template)
+$ tempest new cyclowns/tempest
+... (clones tempest repo)
+```
---
+++ src/main.py
@@ -0,0 +1,20 @@
+# TODO Licensing
+#      Get users preferred license from list (agpl, apache, etc)
+#      Get desired name of file
+# TODO Getting templates
+#      Allow users to create templates based on directory?
+#      '[name]/[repo]' format allows to pull from GitHub/GitLab
+#      '[template]' format pulls from base template list
+#           Created from user-made templates/default templates (npm, rust, etc)
+# TODO Creation
+#      Go down checklist--README.md, license, necessary config files (i.e. cargo.toml)
+#      Create git repo
+#      Prompt name/description of project--these are put into directory name/
+#      README.md respectively
+#      Macros? i.e. putting {NAME} in a file causes it to be replaced with the project name
+
+def get_license():
+    pass
+
+if __name__ == "__main__":
+    pass
